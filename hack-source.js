409a05c3146e8ef38d43d937f55c04c46c8a6fe4
Sean Morris <640101+seanmorris@users.noreply.github.com>
2026-05-04 08:46:51 -0400

    Readiness

--- hack-source.js
+++ hack-source.js
@@ -1,38 +1,91 @@
 import { Client, Server } from 'quickbus';
 
+let executeVSCodeCommandPromise;
+
+const getExecuteVSCodeCommand = () => {
+	if(executeVSCodeCommandPromise)
+	{
+		return executeVSCodeCommandPromise;
+	}
+
+	const getWindowCommandExecutor = async () => {
+		if(window.vscodeEditor?.commands?.executeCommand)
+		{
+			return window.vscodeEditor.commands.executeCommand.bind(window.vscodeEditor.commands);
+		}
+
+		const vscodeEditor = await window.vscodeEditorReady;
+
+		if(vscodeEditor?.commands?.executeCommand)
+		{
+			return vscodeEditor.commands.executeCommand.bind(vscodeEditor.commands);
+		}
+
+		throw new Error('The VS Code command bridge became ready without a commands API.');
+	};
+
+	executeVSCodeCommandPromise = new Promise((resolve, reject) => {
+		const finishWithFallback = error => {
+			getWindowCommandExecutor().then(resolve, fallbackError => reject(error || fallbackError));
+		};
+
+		if(typeof window.require !== 'function')
+		{
+			finishWithFallback();
+			return;
+		}
+
+		try
+		{
+			window.require(
+				['vs/workbench/browser/web.factory']
+				, module => {
+					const executeCommand = module?.commands?.executeCommand;
+
+					if(typeof executeCommand === 'function')
+					{
+						resolve(executeCommand);
+						return;
+					}
+
+					finishWithFallback();
+				}
+				, finishWithFallback
+			);
+		}
+		catch(error)
+		{
+			finishWithFallback(error);
+		}
+	});
+
+	return executeVSCodeCommandPromise;
+};
+
 const fileBusHack = config => {
     const searchParams = new URLSearchParams(location.search);
     const callbackOrigin = searchParams.get('origin');
 
     const client = Client.forWindow(window.parent ?? window.opener, callbackOrigin);
     const server = new Server({
-        openFile: (path, options = {}) => {
-            if(!window.vscodeEditor)
-            {
-                return;
-            }
+        openFile: async (path, options = {}) => {
+            const executeCommand = await getExecuteVSCodeCommand();
 
-            return window.vscodeEditor.commands.executeCommand(
+            return executeCommand(
                 'fileBus.openFile'
                 , 'busfs://' + path
                 , options
             );
         },
-        configure: options => {
-            if(!window.vscodeEditor)
-            {
-                return;
-            }
+        configure: async options => {
+            const executeCommand = await getExecuteVSCodeCommand();
 
-            return window.vscodeEditor.commands.executeCommand('fileBus.configure', options);
+            return executeCommand('fileBus.configure', options);
         },
-        executeCommand: (command, ...args) => {
-            if(!window.vscodeEditor)
-            {
-                return;
-            }
+        executeCommand: async (command, ...args) => {
+            const executeCommand = await getExecuteVSCodeCommand();
 
-            window.vscodeEditor.commands.executeCommand(command, ...args);
+            return executeCommand(command, ...args);
         },
     }, callbackOrigin);
 
